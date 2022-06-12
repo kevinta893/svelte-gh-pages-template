3c17b56c100fb09ffdeba33d4cfef31014b0ee31
kevinta893 <undefined>
2022-06-12 09:20:56 +0900

    Updates

---
+++ _app/immutable/pages/index.svelte-bae6bbb5.js
@@ -0,0 +1,5 @@
+import{S as y,i as G,s as K,e as _,t as o,k as T,c as x,a as k,h as n,d as i,m as V,b as W,g as m,J as h,n as S}from"../chunks/index-c3650d4a.js";function q(E){let e,f,r,s,d,l,u,v,c;return{c(){e=_("h1"),f=o("Welcome to SvelteKit"),r=T(),s=_("p"),d=o("Visit "),l=_("a"),u=o("kit.svelte.dev"),v=o(" to read the documentation"),c=o(`
+
+This is Github pages!`),this.h()},l(t){e=x(t,"H1",{});var a=k(e);f=n(a,"Welcome to SvelteKit"),a.forEach(i),r=V(t),s=x(t,"P",{});var p=k(s);d=n(p,"Visit "),l=x(p,"A",{href:!0});var b=k(l);u=n(b,"kit.svelte.dev"),b.forEach(i),v=n(p," to read the documentation"),p.forEach(i),c=n(t,`
+
+This is Github pages!`),this.h()},h(){W(l,"href","https://kit.svelte.dev")},m(t,a){m(t,e,a),h(e,f),m(t,r,a),m(t,s,a),h(s,d),h(s,l),h(l,u),h(s,v),m(t,c,a)},p:S,i:S,o:S,d(t){t&&i(e),t&&i(r),t&&i(s),t&&i(c)}}}class C extends y{constructor(e){super(),G(this,e,null,q,K,{})}}export{C as default};

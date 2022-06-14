fca390c0cb564983a967131f276830d5443b8c32
kevinta893 <kevinta893@gmail.com>
2022-06-14 19:55:08 +0900

    💄 Minor changes to base page.

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,10 +1,14 @@
 <script lang="ts">
-  import Counter from '$lib/Counter.svelte'
+  import Counter from "$lib/Counter.svelte";
 </script>
 
 <h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<p>
+  Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation
+</p>
 
 This is Github pages!
 
-<Counter></Counter>
+<div>
+  <Counter />
+</div>

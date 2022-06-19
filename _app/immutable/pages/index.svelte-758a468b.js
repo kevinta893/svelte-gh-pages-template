f4750c34166926b1eb1eaab48e0b5fdf7a3cfecc
kevinta893 <undefined>
2022-06-19 11:41:25 +0900

    Updates

---
+++ _app/immutable/pages/index.svelte-758a468b.js
@@ -0,0 +1,9 @@
+import{S as K,i as V,s as G,e as v,t as f,c as $,a as b,h as m,d as i,g as _,J as p,K as W,j as g,n as S,k as j,x as q,m as w,y as z,b as A,z as B,r as D,p as H,C as I}from"../chunks/index-46293cf0.js";function J(c){let e,o,a,s,d;return{c(){e=v("button"),o=f("count is "),a=f(c[0])},l(n){e=$(n,"BUTTON",{});var r=b(e);o=m(r,"count is "),a=m(r,c[0]),r.forEach(i)},m(n,r){_(n,e,r),p(e,o),p(e,a),s||(d=W(e,"click",c[1]),s=!0)},p(n,[r]){r&1&&g(a,n[0])},i:S,o:S,d(n){n&&i(e),s=!1,d()}}}function N(c,e,o){let a=0;return[a,()=>{o(0,a+=1)}]}class O extends K{constructor(e){super(),V(this,e,N,J,G,{})}}function P(c){let e,o,a,s,d,n,r,y,k,h,u,x;return u=new O({}),{c(){e=v("h1"),o=f("Welcome to SvelteKit"),a=j(),s=v("p"),d=f("Visit "),n=v("a"),r=f("kit.svelte.dev"),y=f(" to read the documentation"),k=f(`\r
+\r
+This is Github pages!\r
+\r
+`),h=v("div"),q(u.$$.fragment),this.h()},l(t){e=$(t,"H1",{});var l=b(e);o=m(l,"Welcome to SvelteKit"),l.forEach(i),a=w(t),s=$(t,"P",{});var E=b(s);d=m(E,"Visit "),n=$(E,"A",{href:!0});var T=b(n);r=m(T,"kit.svelte.dev"),T.forEach(i),y=m(E," to read the documentation"),E.forEach(i),k=m(t,`\r
+\r
+This is Github pages!\r
+\r
+`),h=$(t,"DIV",{});var C=b(h);z(u.$$.fragment,C),C.forEach(i),this.h()},h(){A(n,"href","https://kit.svelte.dev")},m(t,l){_(t,e,l),p(e,o),_(t,a,l),_(t,s,l),p(s,d),p(s,n),p(n,r),p(s,y),_(t,k,l),_(t,h,l),B(u,h,null),x=!0},p:S,i(t){x||(D(u.$$.fragment,t),x=!0)},o(t){H(u.$$.fragment,t),x=!1},d(t){t&&i(e),t&&i(a),t&&i(s),t&&i(k),t&&i(h),I(u)}}}class U extends K{constructor(e){super(),V(this,e,null,P,G,{})}}export{U as default};
